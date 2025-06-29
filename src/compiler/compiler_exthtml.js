00288ef82c4c2bb0612463fd9ff890a5cd7a20ef
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-06-28 21:51:12 -0300

    wip: exthtml compiler

--- src/compiler/compiler_exthtml.js
+++ src/compiler/compiler_exthtml.js
@@ -13,12 +13,12 @@ export function exthtmlCompile(source_code_content) {
     const ast = parse(source_code_content);
     let { scripts = [], exthtml = [], styles = [] } = extract_sfc_contents_parts(ast)
 
-
+console.log(inspect(exthtml, { depth: null, colors: true, showHidden: true }));
     let parsedOutput = parseScriptsAndStylesTags(scripts, styles)
     scripts = parsedOutput[0]
     styles = parsedOutput[1]
 
-    const analysis = analyse(scripts)
+    const analysis = analyse(exthtml,scripts)
     return [scripts, exthtml, styles]
 
 
@@ -43,25 +43,26 @@ function parseScriptsAndStylesTags(scripts, styles) {
     }
     for (let x = 0; x < styles.length; x++) {
         styles[x].children = parseStyle(styles[x])
-
     }
 
     return [scripts, styles]
 }
 
 
-function analyse(ast) {
+function analyse(exthtml,scripts) {
+    let ast = scripts
     const result = {
         variables: new Set(),
         willChange: new Set(),
         willUseInTemplate: new Set(),
+        reactiveDeclarations: {}
     }
 
     const reactiveDeclarations = []
     const toRemove = new Set()
 
     for (let x = 0; x < ast.length; x++) {
-        const { scope, map, globals } = periscopic.analyze(ast[x].children);
+        const { scope, map, globals } = periscopic.analyze(ast[x].children)
         result.variables = new Set(scope.declarations.keys())
 
         ast[x].children.body.forEach((node, index) => {
@@ -89,17 +90,125 @@ function analyse(ast) {
                 reactiveDeclarations.push(reactiveDeclaration);
             }
         });
-        ast[x].children.body = ast[x].children.body.filter((node) => !toRemove.has(node));
-        result.reactiveDeclarations = reactiveDeclarations;
-/*
-        console.log(inspect(scope, { depth: null, colors: true, showHidden: true }));
-        console.log(inspect(map, { depth: null, colors: true, showHidden: true }));
-        console.log(inspect(globals, { depth: null, colors: true, showHidden: true }));
-*/
-console.log(inspect(result, { depth: null, colors: true, showHidden: true }));
+        ast[x].children.body = ast[x].children.body.filter((node) => !toRemove.has(node))
+        result.reactiveDeclarations = reactiveDeclarations
+
+        let currentScope = scope
+        estreewalker.walk(ast[x].children, {
+            enter(node) {
+                if (map.has(node)) currentScope = map.get(node);
+                if (
+                    node.type === 'UpdateExpression'
+                    ||
+                    node.type === 'AssignmentExpression'
+                ) {
+                    const names = periscopic.extract_names(
+                        node.type === 'UpdateExpression' ? node.argument : node.left
+                    );
+                    for (const name of names) {
+                        if (
+                            currentScope.find_owner(name) === rootScope
+                            ||
+                            globals.has(name)
+                        ) {
+                            result.willChange.add(name);
+                        }
+                    }
+                }
+            },
+            leave(node) {
+                if (map.has(node)) currentScope = currentScope.parent;
+            },
+        });
+
+        exthtml.forEach(node => traverseExthtml(node))
+
+        /*
+                console.log(inspect(scope, { depth: null, colors: true, showHidden: true }));
+                console.log(inspect(map, { depth: null, colors: true, showHidden: true }));
+                console.log(inspect(globals, { depth: null, colors: true, showHidden: true }));
+        */
+        console.log(inspect(result, { depth: null, colors: true, showHidden: true }));
     }
 
 }
+
+function traverseExthtml(exthtml){
+    switch(exthtml.type){
+        case 'NEW_LINE':
+        case 'SINGLE_LINE_COMMENT':
+        case 'MULTIPLE_LINE_COMMENT':
+        case 'COMMENT_TEXT':
+        case 'SCRIPT_TAG':
+        case 'STYLE_TAG':
+        case 'TEXT_NODE':
+            return
+        case 'DYNAMIC_TEXT_NODE':
+            exthtml.value
+            return
+    }
+    exthtml.children.forEach( node => traverseExthtml(node))
+    exthtml.dynamic_attrs.forEach( dynamicAttr => traverseExthtmlAttr(dynamicAttr))
+    exthtml.event_attrs.forEach( eventAttr => traverseExthtmlEventAttr(eventAttr))
+}
+
+function traverseExthtmlAttr(dynamicAttr){
+    switch(dynamicAttr.category){
+        case "html_global_boolean_attribute":
+        case "html_boolean_attribute":
+            htmlBooleanAttr(dynamicAttr)
+        break;
+        case "html_data_attribute":
+            htmlDataAttr(dynamicAttr)
+        case "html_global_non_boolean_attribute":
+        case "html_attribute":
+            htmlRegularAttr(dynamicAttr)
+        case "html_media_readonly":
+        case "html_video_readonly":
+            htmlReadOnlyAttr(dynamicAttr)
+        case "custom_attribute":
+            htmlMacroAttr(dynamicAttr)
+        case "drall_directive":
+            htmlDrallDirective(dynamicAttr)
+        case "macro_directive":
+            htmlMacroDirective(dynamicAttr)
+    }
+}
+
+function traverseExthtmlEventAttr(eventAttr){
+
+}
+
+
+function htmlBooleanAttr(dynamicAttr){
+
+}
+
+function htmlDataAttr(dynamicAttr){
+
+}
+
+function htmlRegularAttr(dynamicAttr){
+    //class
+    //style
+}
+
+function htmlReadOnlyAttr(dynamic_attrs){
+
+}
+
+function htmlMacroAttr(dynamicAttr){
+
+}
+
+function htmlDrallDirective(dynamicAttr){
+
+}
+
+function htmlMacroDirective(dynamicAttr){
+
+}
+
 
 function generate4Web(ast, analysis) {
 

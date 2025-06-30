1ff7bb223e95f9f5abd7a11b9da68502499aa9c8
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-06-29 21:28:40 -0300

    docs: Add some code examples

---
+++ docs/v0.05/code_examples/home_broker/component/Stock/Stock.go
@@ -0,0 +1,23 @@
+//Seria importado pelo Business da page, onde seria utilizado e contextualizado
+//Como seria se usássemos o mesmo components multiplas vezes?
+
+//Component stock.go
+var Price float
+var OpeningPrice float
+var Change float
+var Ticker string
+var BestOfferBuy string
+var BestOfferSell string
+
+func http_handler(){
+    Ticker := r.URL.Query().Get("Ticker")
+}
+
+func main () {
+    
+}
+
+//HTTP
+//WS
+func Load(Ticker string) {
+}
---
+++ docs/v0.05/code_examples/home_broker/page/home_broker.go
@@ -0,0 +1,29 @@
+package business
+
+//Page
+var MarketStatus string = "Fechado"
+var VWAPPrice string
+var MarketCap string
+var ClosingPrice string
+
+func main(){
+    //Analisa se o mercado está aberto
+    MarketStatus = "Aberto"
+}
+
+/*
+GET => id
+    SEARCH
+        quais index criar no banco dado
+
+    table.campo
+    table.campo2
+
+SAVE
+
+GET
+PUT
+POST
+PATCH
+DELETE
+*/

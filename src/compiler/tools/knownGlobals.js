c4630fe2b6baeaba8d90e247086d52752204fb5e
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-07-27 22:18:51 -0300

    feat: Add knowsGlobals library to compiler

---
+++ src/compiler/tools/knownGlobals.js
@@ -0,0 +1,18 @@
+export const functions = new Set([
+  'alert',
+  'confirm',
+  'prompt',
+  'setTimeout',
+  'setInterval',
+  'clearTimeout',
+  'clearInterval',
+  'fetch',
+  'encodeURI',
+  'decodeURI',
+  'encodeURIComponent',
+  'decodeURIComponent',
+]);
+
+export const objects = new Set([
+  'console',
+])

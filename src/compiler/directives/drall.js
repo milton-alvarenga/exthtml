56c32afc993ef7cf9abfb52fa2cbc4b0a83358eb
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-08-09 12:20:36 -0300

    feat: Add support to extract relevant js parts on drall directives

--- src/compiler/directives/drall.js
+++ src/compiler/directives/drall.js
@@ -1,4 +1,5 @@
 import { addDirective } from './../internals/directive.js'
+import {extract_relevant_js_parts_evaluated_to_string} from './../compiler_exthtml.js'
 
 // Re-export addDirective for reuse
 export { addDirective }
@@ -7,36 +8,42 @@ export { addDirective }
 function perm(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 
 function permGroup(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 
 function permMirror(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 
 function permRedirect(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 
 function val(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 
 function mask(attr,mode,result,variableName,parent_nm) {
     if( mode == "STATIC") {
     } else {
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
     }
 }
 

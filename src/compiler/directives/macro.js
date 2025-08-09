947730c2526ceb3163de2650321e2df2e14b8b31
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-08-09 12:20:27 -0300

    feat: Add support to extract relevant js parts on macro directives

--- src/compiler/directives/macro.js
+++ src/compiler/directives/macro.js
@@ -1,4 +1,5 @@
 import { addDirective } from './../internals/directive.js'
+import {extract_relevant_js_parts_evaluated_to_string} from './../compiler_exthtml.js'
 
 // Re-export addDirective for reuse
 export { addDirective }
@@ -9,11 +10,12 @@ function idname(attr,mode,result,variableName,parent_nm) {
     result.code.internal_import.add("setAttr")
     
     if( mode == "STATIC") {
-        result.code.create.push(`setAttr('${variableName}', 'id', '${attr.value}')`)
-        result.code.create.push(`setAttr('${variableName}', 'name', '${attr.value}')`)
+        result.code.create.push(`setAttr(${variableName}, 'id', '${attr.value}')`)
+        result.code.create.push(`setAttr(${variableName}, 'name', '${attr.value}')`)
     } else {
-        result.code.update.push(`setAttr('${variableName}', 'id', ${attr.value})`)
-        result.code.update.push(`setAttr('${variableName}', 'name', ${attr.value})`)
+        extract_relevant_js_parts_evaluated_to_string(attr.value, result)
+        result.code.update.push(`setAttr(${variableName}, 'id', ${attr.value})`)
+        result.code.update.push(`setAttr(${variableName}, 'name', ${attr.value})`)
     }
 }
 

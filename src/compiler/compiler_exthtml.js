f4eb0c815ab5e480ce9e8453574ae9e46e8d32dc
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-08-18 21:45:05 -0300

    fix: Code support to while edit the input and it has been reactivily changed, to keep cursor position

--- src/compiler/compiler_exthtml.js
+++ src/compiler/compiler_exthtml.js
@@ -724,11 +724,11 @@ function htmlRegularAttr(attr, mode, result, variableName, node, parent_nm) {
 
         result.code.reactives.push(`function ${reactiveFnName}(){
             if ('${node.value}' == 'INPUT' && ${attr.value} && document.activeElement === ${variableName}) {
-                const cursorPosition = input.selectionStart
+                const cursorPosition = ${variableName}.selectionStart
             }
             (${attr.value}) ? setAttr(${variableName}, '${attr.name}', ${attr.value}) : rmAttr('${variableName}', '${attr.name}')
             if ('${node.value}' == 'INPUT' && ${attr.value} && document.activeElement === ${variableName}) {
-                    input.setSelectionRange(cursorPosition, cursorPosition)
+                    ${variableName}.setSelectionRange(cursorPosition, cursorPosition)
             }
         }`)
 

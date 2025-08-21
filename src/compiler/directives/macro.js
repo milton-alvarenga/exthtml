f10d71810a9e99bbd751824c8589475ccb32a614
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-08-20 21:44:25 -0300

    refactor: Internal function add $$_ on directives macro

--- src/compiler/directives/macro.js
+++ src/compiler/directives/macro.js
@@ -10,8 +10,8 @@ function idname(attr,mode,result,variableName,node, parent_nm) {
     result.code.internal_import.add("setAttr")
     
     if( mode == "STATIC") {
-        result.code.create.push(`setAttr(${variableName}, 'id', '${attr.value}')`)
-        result.code.create.push(`setAttr(${variableName}, 'name', '${attr.value}')`)
+        result.code.create.push(`$$_setAttr(${variableName}, 'id', '${attr.value}')`)
+        result.code.create.push(`$$_setAttr(${variableName}, 'name', '${attr.value}')`)
     } else {
         let reactiveFnName = `${variableName}__idname`
         let usedVars = extract_relevant_js_parts_evaluated_to_string(attr.value, result)
@@ -22,8 +22,8 @@ function idname(attr,mode,result,variableName,node, parent_nm) {
             result.code.dependencyTree.push(`$$_depVar.dependents.directives.add(${reactiveFnName})`)
         }
         result.code.reactives.push(`function ${reactiveFnName}(){\n
-            setAttr(${variableName}, 'id', ${attr.value});\n
-            setAttr(${variableName}, 'name', ${attr.value});\n
+            $$_setAttr(${variableName}, 'id', ${attr.value});\n
+            $$_setAttr(${variableName}, 'name', ${attr.value});\n
         }`)
 
         result.code.create.push(`${reactiveFnName}()`)

fea6b109966b4d721b1a8716cee63e70f2fcead7
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-10-20 20:52:10 -0300

    feat: restore virtual import as filename static

--- src/compiler/compiler_exthtml.js
+++ src/compiler/compiler_exthtml.js
@@ -126,8 +126,8 @@ function analyse(exthtml, scripts, styles, filePath) {
                 result.code.internal_import.add('removeCssLinkFromHead')
                 result.code.create.push(`$$_addCssLinkOnHead('/${baseName}')`)
                 result.code.destroy.push(`$$_removeCssLinkFromHead('/${baseName}')`)
-                //result.code.imports.push(`import 'virtual:${baseName}'`)
-                result.code.imports.push(`//import 'virtual'`)
+                result.code.imports.push(`import 'virtual:${baseName}'`)
+                //result.code.imports.push(`//import 'virtual'`)
             } else {
                 //Must to be inline css as we did not know the component path
                 result.code.internal_import.add("el")

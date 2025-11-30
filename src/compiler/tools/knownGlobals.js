3e3faed7c04346c91e0eb15ff3008ddd763fda6e
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-11-30 17:36:21 -0300

    feat: Improve js knownGlobals

--- src/compiler/tools/knownGlobals.js
+++ src/compiler/tools/knownGlobals.js
@@ -1,18 +1,44 @@
 export const functions = new Set([
+  //Dialog
   'alert',
   'confirm',
   'prompt',
+  //Timers
   'setTimeout',
   'setInterval',
   'clearTimeout',
   'clearInterval',
+  //Network
   'fetch',
+  //URI encoding
   'encodeURI',
   'decodeURI',
   'encodeURIComponent',
   'decodeURIComponent',
+  //Data encoding
+  'atob',
+  'btoa',
+  //Task scheduling
+  'queueMicrotask'
 ]);
 
+export const classes = new Set([
+  'AbortController',
+  'AbortSignal',
+  'Headers',
+  'Request',
+  'Response',
+  'URL',
+  'URLSearchParams',
+  'TextEncoder',
+  'TextDecoder'
+])
+
 export const objects = new Set([
-  'console'
+  'console',
+  'crypto',
+  'document',
+  'location',
+  'navigator',
+  'window'
 ])

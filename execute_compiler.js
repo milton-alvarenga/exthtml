eb5dce4d739dff98a1e2ada336b7c30fec85b8ce
alvarenga.milton <alvarenga.milton@drall.com.br>
2025-11-30 22:38:42 -0300

    feat: add reactive for variable method called

--- execute_compiler.js
+++ execute_compiler.js
@@ -42,8 +42,19 @@ const __dirname = dirname(__filename);
 //const filePathIfNoneInformed = './src/examples/exthtml/var_reactivity/var_reactivity.exthtml';
 //const filePathIfNoneInformed = './src/examples/exthtml/input_reactivity_with_event/input_reactivity_with_event.exthtml'
 //const filePathIfNoneInformed = './src/examples/exthtml/var_reactivity_with_logic/var_reactivity_with_logic.exthtml'
-const filePathIfNoneInformed = './src/examples/exthtml/svelte_based/vars/samples/imports/index.exthtml'
+//const filePathIfNoneInformed = './src/examples/exthtml/svelte_based/vars/samples/imports/index.exthtml'
 //const filePathIfNoneInformed = './src/examples/exthtml/svelte_based/vars/samples/duplicate-non-hoistable/index.exthtml'
+//const filePathIfNoneInformed = './src/examples/exthtml/svelte_based/vars/samples/modular-vars/index.exthtml'
+//const filePathIfNoneInformed = './src/examples/exthtml/script_inside_html/script_code_inside_component.exthtml'
+//const filePathIfNoneInformed = './src/examples/exthtml/macro_attribute/idname_attribute.exthtml'
+//const filePathIfNoneInformed = './src/examples/exthtml/lang_attribute/if/simple_div.exthtml'
+//const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/if_directive/simple_and_direct_before/index.exthtml'
+//const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/if_directive/double_ifs_conditional/index.exthtml'
+//const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/for_directive/simple_array/index.exthtml'
+//const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/if_directive/nested_ifs/index.exthtml'
+//const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/for_directive/simple_array/index.exthtml'
+const filePathIfNoneInformed = '../exthtml_vite_plugin/tests/kindergarten/for_directive/simple_array_dynamic/index.exthtml'
+
 
 const args = process.argv.slice(2);
 
@@ -67,7 +78,7 @@ async function main() {
     //console.log(generate_code)
     //fs.writeFileSync('output_ctx.js', generated_ctx);
     fs.writeFileSync('output.js', generate_code);
-    fs.writeFileSync(path.basename(filePath, path.extname(filePath))+".js", generate_code);
+    //fs.writeFileSync(path.basename(filePath, path.extname(filePath))+".js", generate_code);
   } catch (err) {
     console.error('Error reading file:', err);
   }
